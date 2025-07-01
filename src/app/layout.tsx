471f1231bd345d81e27672f290c448f313b5a11f
ayush68824 <ayush.kumar2021@vitbhopal.ac.in>
2025-07-01 21:21:39 +0530

    fix: wrap app with Redux Provider to prevent SSR/SSG store errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
+        <Provider store={store}>
+          {children}
+        </Provider>
       </body>
     </html>
   );
